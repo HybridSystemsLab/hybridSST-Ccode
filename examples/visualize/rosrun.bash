5859c9de1f4e33739ea3876c7ccb8910eace03ae
Beverly Xu <73717685+xu21beve@users.noreply.github.com>
2024-11-21 21:40:34 -0800

    Update rosrun.bash

--- examples/visualize/rosrun.bash
+++ examples/visualize/rosrun.bash
@@ -4,9 +4,11 @@ echo "Enter index (0-) of the desired y-coordinates: "
 read y_index
 echo "(Optional) Enter index (0-) of the desired z-coordinates. If no z-coordinates are desired, press return: "
 read z_index
+echo "Show obstacles? [y/n]: "
+read obstacles
 
 echo $x_index $y_index $z_index > "coordinate_indices.txt"
-source /opt/ros/humble/setup.bash
+echo $obstacles > "obstacles.txt"
 colcon build --packages-select visualize
 source install/local_setup.bash
 ros2 run visualize ros_visualize
