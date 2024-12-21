da41a3cd31c2f4775673285ab2ef93d7b7b098e2
Beverly Xu <xu21beve@gmail.com>
2024-12-21 13:09:49 -0800

    add scripts to generate data for figure 6

---
+++ examples/figure6.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Enter desired solution batch size (1-7): "
+read batch_size
+while ! [[ "$batch_size" =~ ^[1-7]$ ]]; do
+    echo "Batch size must be an integer between 1 and 7. Please try again: "
+    read batch_size
+done
+
+if [ "$batch_size" -eq 1 ]; then
+    ./run1
+elif [ "$batch_size" -eq 2 ]; then
+    ./run2
+elif [ "$batch_size" -eq 3 ]; then
+    ./run3
+elif [ "$batch_size" -eq 4 ]; then
+    ./run4
+elif [ "$batch_size" -eq 5 ]; then
+    ./run5
+elif [ "$batch_size" -eq 6 ]; then
+    ./run6
+elif [ "$batch_size" -eq 7 ]; then
+    ./run7
+fi
